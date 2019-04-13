b5957d64e4ef5ed0baeca69080eddb9ff871a3b5
Mohamed Faesal <mohamed.feasal@gmail.com>
2019-04-13 22:06:11 +0200

    Create authentication entity that contains credentials for chat-api

---
+++ src/Entity/Authentication.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ChatApi/Entity;
+
+/**
+ * Authentication class entity that contains chat api credentials
+ * @package ChatApi/Entity
+ * @author Mohamed Faesal <mohamed.feasal@gmail.com>
+ **/
+class Authentication
+{
+  /**
+   * @var string chat-api token
+   */
+  public $token;
+  
+  /**
+   * @var string chat-api url will call at
+   */
+  public $url;  
+
+  /**
+   * Authentication contructor
+   * @var string $token chat-api token
+   * @var string $url   chat-api url
+   */
+  public function __construct(string $token, string $url)
+  {
+    $this->token = $token;
+    $this->url   = $url;
+  }
+}
